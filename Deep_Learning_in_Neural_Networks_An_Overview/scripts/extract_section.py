c8c44087c4cc1740171c4bad2a84cba1a34c5926
guanfuchen <guanfuchen@zju.edu.cn>
2017-12-02 15:34:19 +0800

    update

--- Deep_Learning_in_Neural_Networks_An_Overview/scripts/extract_section.py
+++ Deep_Learning_in_Neural_Networks_An_Overview/scripts/extract_section.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 tex_header = open('tex_header.tex').read()
 tex_tail = open('tex_tail.tex').read()
@@ -10,7 +11,15 @@ tex = open('DeepLearning8Oct2014.tex').read()
 section = re.findall(r'\\section(.*?)%\\newpage', tex, re.S)
 #  print(len(section))
 #  print(section[0])
-for section_index in len(section):
-    if section_index > 0:
-        section_tex = tex_header + tex + tex_tail
+for section_index in range(len(section)):
+    if section_index >= 0:
+        section_tex = tex_header + section[section_index] + tex_tail
+        chapter_index = section_index + 1
+        chapter_dir = 'Chapter' + str(chapter_index)
+        #  print(chapter_dir)
+        if not os.path.isdir(chapter_dir):
+            os.mkdir(chapter_dir)
+        chapter_section_file = open(os.path.join(chapter_dir, chapter_dir+'.tex'), 'w')
+        chapter_section_file.write(section_tex)
+        chapter_section_file.close()
 
